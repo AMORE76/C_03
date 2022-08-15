3e7dc23682dd21d39ad751b822c6d4d2184b3e47
AMORE76 <gemenius007@internet.ru>
2022-08-15 18:09:46 +0300

    Solved  HomeTask_005

--- HomeTask_005/Program.cs
+++ HomeTask_005/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 27: Напишите программу, которая принимает на вход число 
+и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12*/
+
+Console.WriteLine("Input integer number ()");
+int N = Convert.ToInt32(Console.ReadLine());
+
+N = Math.Abs(N);
+
+double SumOfDigit(int A)
+
+{
+    double sum = 0;
+    while (A != 0)
+    {
+        sum += A % 10;
+        A /= 10;
+    }
+    double result = Convert.ToInt32(sum);
+    return result;
+}
+
+double DigitSum = SumOfDigit(N);
+
+Console.WriteLine(DigitSum);
