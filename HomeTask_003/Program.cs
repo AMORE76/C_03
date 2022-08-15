91744c5c0643b30c1bea37fb1c5ef1e51980271e
AMORE76 <gemenius007@internet.ru>
2022-08-15 21:50:39 +0300

    Changed decision of task in  HomeTask_003

--- HomeTask_003/Program.cs
+++ HomeTask_003/Program.cs
@@ -8,7 +8,28 @@
 3 -> 1, 8, 27
 5 -> 1, 8, 27, 64, 125*/
 
-Console.WriteLine("Input integer number ()");
+/*Console.WriteLine("Input two integer numbers ()");
+
+int Number = Convert.ToInt32(Console.ReadLine());
+int Degree = Convert.ToInt32(Console.ReadLine());*/
+
+double Exposure(double A, double b)
+{
+    double result = Convert.ToInt32(Math.Pow(A, b));
+    return result;
+}
+
+
+int N = 5;
+int Degree = 3;
+double m = 1;
+for (int i = 1; i <= N; i++)
+{
+    m = Exposure(i, Degree);
+    Console.Write(" " + m);
+}
+
+/*Console.WriteLine("Input integer number ()");
 int N = Convert.ToInt32(Console.ReadLine());
 
 //int N =3;
@@ -19,5 +40,5 @@ for (int i = 1; i <= N; i++)
 {
     M = Convert.ToInt32(Math.Pow(i, 3));
     Console.Write(" " + M);
-}
+}*/
 
