be79ec4901b291dacb6cdd704ba5bb8ba6007836
AMORE76 <gemenius007@internet.ru>
2022-08-15 14:35:19 +0300

    Solved HomeTask_003

--- HomeTask_003/Program.cs
+++ HomeTask_003/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 23
+
+Напишите программу, которая принимает на вход число (N) 
+и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125*/
+
+Console.WriteLine("Input integer number ()");
+int N = Convert.ToInt32(Console.ReadLine());
+
+//int N =3;
+
+int M = 1;
+
+for (int i = 1; i <= N; i++)
+{
+    M = Convert.ToInt32(Math.Pow(i, 3));
+    Console.Write(" " + M);
+}
+
