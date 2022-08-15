3ba8aa1705d7ecb4dc3103a8e46b86898be19fa0
AMORE76 <gemenius007@internet.ru>
2022-08-15 17:38:19 +0300

    Solved  HomeTask_004

--- HomeTask_004/Program.cs
+++ HomeTask_004/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Урок 4. Функции продолжение
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16
+
+*/
+
+Console.WriteLine("Input two integer numbers ()");
+
+int Number = Convert.ToInt32(Console.ReadLine());
+int Degree = Convert.ToInt32(Console.ReadLine());
+
+double Exposure(double A, double b)
+
+{
+    double result = Convert.ToInt32(Math.Pow(A, b));
+    return result;
+}
+
+double NumberInDegree = Exposure(Number, Degree);
+
+Console.WriteLine(NumberInDegree);
+
