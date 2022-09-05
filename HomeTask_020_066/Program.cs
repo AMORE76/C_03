60eb4afca76f1391d1336521e5b6b1bffdad75b7
AMORE76 <gemenius007@internet.ru>
2022-09-05 15:59:59 +0300

    Solved the task in the folder HomeTask_020_066

---
+++ HomeTask_020_066/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int InputNumber(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+void PrintSum(int m, int n, int sum)
+{
+  sum = sum + n;
+  if (n <= m)
+  {
+    Console.Write($"Сумма элементов = {sum} ");
+    return;
+  }
+  PrintSum(m, n - 1, sum);
+}
+
+
+Console.Clear();
+Console.WriteLine($"Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N");
+int m = InputNumber("Введите меньшее натуральное число M: ");
+int n = InputNumber("Введите меньшее натуральное число N: ");
+int temp = m;
+
+if (m > n) 
+{
+  m = n; 
+  n = temp;
+}
+
+PrintSum(m, n, temp=0);
