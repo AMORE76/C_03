0dc3ca7ff4bda58a6232a4302527688315bc2584
AMORE76 <gemenius007@internet.ru>
2022-09-16 14:39:46 +0300

    Created block of Create of new  array

--- Final_CW/Program.cs
+++ Final_CW/Program.cs
@@ -3,7 +3,31 @@
 Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
 При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.*/
 
-string[] NewArrayByCondition(string [] arr, int  size = 0, int index = 0)
+string[] AddNewValueToArray(string[] arr, string newRow)
+{
+    string[] newArr = new string[arr.Length + 1];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        newArr[i] = arr[i];
+    }
+    newArr[newArr.Length - 1] = newRow;
+    return newArr;
+}
+
+string[] InputArrayByUser()
+{
+    string[] arr = new string[0];
+    while (true)
+    {
+        Console.Write("Введите строку(и) с клавиатуры. После завершения ввода введите с клавиатуры - Q. : ");
+        string userInformation = Console.ReadLine()!;
+        if (userInformation == "q" || userInformation == "Q") break;
+        else arr = AddNewValueToArray(arr, newRow: userInformation);
+    }
+    return arr;
+}
+
+string[] CreateArrayByCondition(string [] arr, int  size = 0, int index = 0)
 {
     for (int i = 0; i < arr.Length; i++)
     {
@@ -21,36 +45,14 @@ string[] NewArrayByCondition(string [] arr, int  size = 0, int index = 0)
     return newArr;
 }
 
-string[] AddingNewValuesToArray(string[] arr, string newRow)
-{
-    string[] newArr = new string[arr.Length + 1];
-    for (int i = 0; i < arr.Length; i++)
-    {
-        newArr[i] = arr[i];
-    }
-    newArr[newArr.Length - 1] = newRow;
-    return newArr;
-}
 
-string[] InputArrayUser()
-{
-    string[] arr = new string[0];
-    while (true)
-    {
-        Console.Write("Для завершения ввода введите - q. Введите строку: ");
-        string userInformation = Console.ReadLine()!;
-        if (userInformation == "q" || userInformation == "Q") break;
-        else arr = AddingNewValuesToArray(arr, newRow: userInformation);
-    }
-    return arr;
-}
 
-Console.WriteLine("Программа, которая принимает строки и возвращает те, длина которых меньше либо равна 3 символам.");
+Console.WriteLine("Программа,принимающая на вход массив строк и возвращающая массив строк длиной меньшей либо равной 3 символам.");
 Console.WriteLine();
 
-string[] information = InputArrayUser();
+string[] initialData = InputArrayByUser();
 Console.WriteLine();
-Console.WriteLine("[{0}] - введенные значения ", string.Join(", ", information));
+Console.WriteLine("[{0}] - исходный массив строк ", string.Join(", ", initialData));
 Console.WriteLine();
-string[] newInformation = NewArrayByCondition(arr: information);
-Console.WriteLine("[{0}] - длина меньше либо равна 3 символам", string.Join(", ", newInformation));
+string[] newData = CreateArrayByCondition(arr: data);
+Console.WriteLine("[{0}] - итоговый массив строк длиной =< 3 символам", string.Join(", ", newInformation));
